e794c132a33e9fe811237e9c2b80dc8f1ff51fa5
sm <seungmin.lee@holiday-robotics.com>
2024-11-15 16:04:58 +0900

    feat: remove episode dataset from rollout pushany

--- custom_datasets/rollout_push_any.py
+++ custom_datasets/rollout_push_any.py
@@ -1,32 +1,46 @@
+from pathlib import Path
 from typing import Optional
 
 import torch
+import zarr
 
 from custom_datasets.core import TrajectoryDataset
-from lerobot.episode_dataset import EpisodeDataset
+from lerobot.image_utils import load_from_frame_torch
+from lerobot.rollout_datasets.zarr_utils import to_hf_dataset
+from lerobot.utils import calculate_episode_data_index
 
 
 class RolloutPushAnyDataset(TrajectoryDataset):
     def __init__(self, data_directory, subset_fraction: Optional[float] = None, relative=False):
         super().__init__()
-        self._lerobot_dataset = EpisodeDataset.from_zarr(data_directory)
+
+        self.root_path = Path(data_directory)
+        self.frame_path = self.root_path / "frames"
+
+        self.root_group = zarr.open(data_directory, mode='r')
+
+        self.hf_dataset = to_hf_dataset(self.root_group)
+        self.episode_data_index = calculate_episode_data_index(self.hf_dataset)
+        self._hf_num_episodes = len(self.hf_dataset.unique("episode_index"))
 
         self.relative = relative
 
         self.subset_fraction = subset_fraction if subset_fraction else 1.0
-        self._num_episodes = int(self._lerobot_dataset.num_episodes * subset_fraction) if subset_fraction \
-            else self._lerobot_dataset.num_episodes
+        self._num_episodes = int(self._hf_num_episodes * self.subset_fraction)
 
     def get_seq_length(self, episode_idx):
         from_idx, to_idx = self._get_episode_from_to(episode_idx)
         return to_idx - from_idx
 
-    def get_frames(self, episode_idx, frames):
-        from_idx, to_idx = self._get_episode_from_to(episode_idx)
-        episode_sequence = self._lerobot_dataset[from_idx:to_idx]
+    def get_frames(self, episode_idx, frame_indices):
+        from_idx, _ = self._get_episode_from_to(episode_idx)
+        frame_indices = [from_idx + i for i in frame_indices]
+        items = self.hf_dataset[frame_indices]
 
-        obs = episode_sequence['observation.image'][frames].unsqueeze(1)
-        act = torch.stack(episode_sequence['action'], dim=0)[frames]
+        items = load_from_frame_torch(items, ['observation.image'], self.frame_path)
+
+        obs = items['observation.image'].unsqueeze(1)
+        act = torch.stack(items['action'], dim=0)
         mask = torch.ones(len(act)).bool()
         return obs, act, mask
 
@@ -37,6 +51,6 @@ class RolloutPushAnyDataset(TrajectoryDataset):
         return self._num_episodes
 
     def _get_episode_from_to(self, episode_idx):
-        from_idx = self._lerobot_dataset.episode_data_index["from"][episode_idx].item()
-        to_idx = self._lerobot_dataset.episode_data_index["to"][episode_idx].item()
+        from_idx = self.episode_data_index["from"][episode_idx].item()
+        to_idx = self.episode_data_index["to"][episode_idx].item()
         return from_idx, to_idx
