aaccc8ddbbe0d34682c3ed5c36e9bb2a2c9da2b1
postBG <profile2697@gmail.com>
2024-12-05 02:20:15 +0900

    feat: dino loss

--- models/ssl/dynamo_dino_reg.py
+++ models/ssl/dynamo_dino_reg.py
@@ -1,22 +1,20 @@
-from typing import Tuple, Dict, Optional
-
 import einops
-import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from accelerate import Accelerator
 
 from .dynamo import DynaMoSSL
-from ..ema import EMA
-from ..transformer_encoder import TransformerEncoder, TransformerEncoderConfig
 
 accelerator = Accelerator()
 
 
 class DINOLoss(nn.Module):
-    def __init__(self, out_dim, warmup_teacher_temp, teacher_temp,
-                 warmup_teacher_temp_epochs, nepochs, student_temp=0.1,
+    def __init__(self, out_dim,
+                 teacher_temp,
+                 # warmup_teacher_temp,
+                 # warmup_teacher_temp_epochs, num_epochs,
+                 student_temp=0.1,
                  center_momentum=0.9):
         super().__init__()
         self.student_temp = student_temp
@@ -24,11 +22,12 @@ class DINOLoss(nn.Module):
         self.register_buffer("center", torch.zeros(1, out_dim))
         # we apply a warm up for the teacher temperature because
         # a too high temperature makes the training instable at the beginning
-        self.teacher_temp_schedule = np.concatenate((
-            np.linspace(warmup_teacher_temp,
-                        teacher_temp, warmup_teacher_temp_epochs),
-            np.ones(nepochs - warmup_teacher_temp_epochs) * teacher_temp
-        ))
+        # self.teacher_temp_schedule = np.concatenate((
+        #     np.linspace(warmup_teacher_temp,
+        #                 teacher_temp, warmup_teacher_temp_epochs),
+        #     np.ones(num_epochs - warmup_teacher_temp_epochs) * teacher_temp
+        # ))
+        self.teacher_temp = teacher_temp
 
     def forward(self, student_output, teacher_output, epoch):
         """
@@ -44,7 +43,8 @@ class DINOLoss(nn.Module):
         student_log_dists = F.log_softmax(student_out, dim=-1)
 
         # teacher centering and sharpening
-        temp = self.teacher_temp_schedule[epoch]
+        # temp = self.teacher_temp_schedule[epoch]
+        temp = self.teacher_temp
         teacher_dists = F.softmax((teacher_output - self.center) / temp, dim=-1).detach()
 
         total_loss = torch.sum(-teacher_dists * student_log_dists, dim=-1).mean()
@@ -62,8 +62,12 @@ class DINOLoss(nn.Module):
 
 
 class DynaMoDinoSSL(DynaMoSSL):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, teacher_temp, student_temp, center_momentum, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.dino_loss = DINOLoss(out_dim=self.feature_dim,
+                                  teacher_temp=teacher_temp,
+                                  student_temp=student_temp,
+                                  center_momentum=center_momentum)
 
     def _forward_dyn_loss_one_pair(
             self,
@@ -75,10 +79,6 @@ class DynaMoDinoSSL(DynaMoSSL):
     ):
         forward_dyn_input = torch.cat([obs_enc[:, :-1, j], obs_proj[:, 1:, i]], dim=-1)
         obs_enc_pred = self.forward_dynamics(forward_dyn_input)  # (N, T-1, E)
-        loss = (
-                1
-                - torch.nn.functional.cosine_similarity(
-            obs_enc_pred, obs_target[:, 1:, j].detach(), dim=-1
-        ).mean()
-        )
+
+        loss = self.dino_loss(obs_enc_pred, obs_target[:, 1:, j].detach())
         return loss
