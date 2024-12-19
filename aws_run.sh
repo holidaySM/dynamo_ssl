6747a7410a062f4efb49ac32a8e02cfc41fcf4c3
sm <seungmin.lee@holiday-robotics.com>
2024-12-19 18:12:46 +0900

    feat: update sh

--- aws_run.sh
+++ aws_run.sh
@@ -1,1 +1,1 @@
-accelerate launch train.py --config-name=train_pusht_rollout_headless_dino encoder=vit_b16_random
+accelerate launch --mixed_precision bf16 train.py --config-name=train_pusht_rollout_headless_dino encoder=vit_b16_random batch_size=128 num_workers=8
