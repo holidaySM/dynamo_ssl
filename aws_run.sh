6aba0fefc00af883798fb24d4d97324056ac36fb
sm <seungmin.lee@holiday-robotics.com>
2024-12-24 00:52:28 +0900

    feat: add run script

--- aws_run.sh
+++ aws_run.sh
@@ -1,1 +1,1 @@
-accelerate launch --mixed_precision bf16 train.py --config-name=train_pusht_rollout_headless_dino encoder=vit_b16_random batch_size=128 num_workers=8
+accelerate launch --mixed_precision bf16 train.py --config-name=train_pusht_rollout_headless_dino encoder=vit_b16_random batch_size=128 num_workers=8 ssl_lr=0.0005
